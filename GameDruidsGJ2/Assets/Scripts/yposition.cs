a5e90b7e3f810c535c409706476d2136c5a58704
FadiMasannat <60350273+FadiMasannat@users.noreply.github.com>
2020-04-04 15:31:13 +0300

    Position
    X and Y positions

---
+++ GameDruidsGJ2/Assets/Scripts/yposition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class yposition : MonoBehaviour
+{
+
+    public Transform player;
+    public Text ypos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ypos.text = player.position.y.ToString("0");
+
+    }
+}
